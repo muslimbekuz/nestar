01c4467139b3eda0ce06742b549c73f5ea9c9157
muslimbekuz <muslimbekkhalimjonov@gmail.com>
2026-09-06 22:29:49 +0900

    feat: integration of lookupAuthMemberLiked complex query business logic

--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -16,7 +16,7 @@ import { PropertyStatus } from '../../libs/enums/property.enum';
 import { ViewGroup } from '../../libs/enums/view.enum';
 import { PropertyUpdate } from '../../libs/dto/property/property.update';
 import moment = require('moment');
-import { lookupMember, shapeIntoMongoObjectId } from '../../libs/config';
+import { lookupAuthMemberLiked, lookupMember, shapeIntoMongoObjectId } from '../../libs/config';
 import { LikeService } from '../like/like.service';
 import { LikeInput } from '../../libs/dto/like/like.input';
 import { LikeGroup } from '../../libs/enums/like.enum';
@@ -115,7 +115,7 @@ export class PropertyService {
 						list: [
 							{ $skip: (input.page - 1) * input.limit },
 							{ $limit: input.limit },
-							// meLiked
+							lookupAuthMemberLiked(memberId),
 							lookupMember,
 							{ $unwind: '$memberData' },
 						],
